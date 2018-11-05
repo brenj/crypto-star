ad835eb6459020504dbd4c00c1dbe3103f295d7f
brenj <brenj@mailbox.org>
2018-11-04 22:54:18 -0600

    Fix tests for buying/selling a star

--- test/StarNotaryTest.js
+++ test/StarNotaryTest.js
@@ -72,33 +72,40 @@ contract('StarNotary', (accounts) => {
     const starPrice = web3.toWei(0.01, 'ether');
 
     beforeEach(async function () {
-      await this.contract.createStar('awesome star!', starId, { from: user1 });
+      await this.contract.createStar(
+        'awesome star #1!', 'dec_121.874', 'mag_245.978', 'ra_032.155',
+        'awesome star story #1', starId, { from: user1 });
     });
 
     it('user1 can put up their star for sale', async function () {
       assert.equal(await this.contract.ownerOf(starId), user1);
-      await this.contract.putStarUpForSale(starId, starPrice, { from: user1 });
+      await this.contract.putStarUpForSale(
+        starId, starPrice, { from: user1 });
 
       assert.equal(await this.contract.starsForSale(starId), starPrice);
     });
 
     describe('user2 can buy a star that was put up for sale', () => {
       beforeEach(async function () {
-        await this.contract.putStarUpForSale(starId, starPrice, { from: user1 });
+        await this.contract.putStarUpForSale(
+          starId, starPrice, { from: user1 });
       });
 
-      it('user2 is the owner of the star after they buy it', async function () {
-        await this.contract.buyStar(starId, { from: user2, value: starPrice, gasPrice: 0 });
+      it('user2 is the owner of star after they buy it', async function () {
+        await this.contract.buyStar(
+          starId, { from: user2, value: starPrice, gasPrice: 0 });
         assert.equal(await this.contract.ownerOf(starId), user2);
       });
 
       it('user2 ether balance changed correctly', async function () {
         const overpaidAmount = web3.toWei(0.05, 'ether');
         const balanceBeforeTransaction = web3.eth.getBalance(user2);
-        await this.contract.buyStar(starId, { from: user2, value: overpaidAmount, gasPrice: 0 });
+        await this.contract.buyStar(
+          starId, { from: user2, value: overpaidAmount, gasPrice: 0 });
         const balanceAfterTransaction = web3.eth.getBalance(user2);
 
-        assert.equal(balanceBeforeTransaction.sub(balanceAfterTransaction), starPrice);
+        assert.equal(
+          balanceBeforeTransaction.sub(balanceAfterTransaction), starPrice);
       });
     });
   });
