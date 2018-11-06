29ede754f25eb0f6ed8dfa205d2f4dfd30bd8133
brenj <brenj@mailbox.org>
2018-11-06 11:11:47 -0600

    Update truffle configuration files
    Update to deploy to rinkeby network.

--- truffle-config.js
+++ truffle-config.js
@@ -1,18 +1,21 @@
-/*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
- * function when declaring them. Failure to do so will cause commands to hang. ex:
- * ```
- * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
- *     },
- *     network_id: '1',
- *     gas: 4500000,
- *     gasPrice: 10000000000,
- *   },
- */
+var HDWalletProvider = require('truffle-hdwallet-provider');
+
+var mnemonic = 'foster scene ethics bacon trap win purpose dynamic worry danger negative page';
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: "*"
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/dc8f7b316a51477aa95b90492a7334fd')
+      },
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000,
+    }
+  }
 };
